b35a7d2117d73976b3cd18e69d2a7d10ef23b31c
Chinmay Rawool <chinmay811@gmail.com>
2017-03-10 22:40:01 -0500

    Audio play

--- app/src/main/java/com/mad/tedradiohourpodcast/ListAdapter.java
+++ app/src/main/java/com/mad/tedradiohourpodcast/ListAdapter.java
@@ -1,16 +1,20 @@
 package com.mad.tedradiohourpodcast;
 
 import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -20,6 +24,7 @@ import java.util.ArrayList;
 public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
     ArrayList<Itunes> itunesList;
     Context mContext;
+    MediaPlayer mPlayer;
 
     public ListAdapter(ArrayList<Itunes> itunesList, Context context) {
         this.itunesList = itunesList;
@@ -53,10 +58,32 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
         ImageView iv = (ImageView) holder.view.findViewById(R.id.iv_image);
         tv.setText(itunesList.get(position).getTitle()+"\n"+"Posted: "+itunesList.get(position).getDate());
         Picasso.with(mContext).load(itunesList.get(position).getImgUrl()).into(iv);
+        LinearLayout linear = (LinearLayout) holder.view.findViewById(R.id.linear1);
         holder.view.findViewById(R.id.iv_play_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 display(position);
+                mPlayer = new MediaPlayer();
+                mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                try {
+                    mPlayer.setDataSource(itunesList.get(position).getMp3Url());
+                } catch (IllegalArgumentException e) {
+                    Toast.makeText(mContext, "You might not set the URI correctly!", Toast.LENGTH_LONG).show();
+                } catch (SecurityException e) {
+                    Toast.makeText(mContext, "You might not set the URI correctly!", Toast.LENGTH_LONG).show();
+                } catch (IllegalStateException e) {
+                    Toast.makeText(mContext, "You might not set the URI correctly!", Toast.LENGTH_LONG).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    mPlayer.prepare();
+                } catch (IllegalStateException e) {
+                    Toast.makeText(mContext, "You might not set the URI correctly!", Toast.LENGTH_LONG).show();
+                } catch (IOException e) {
+                    Toast.makeText(mContext, "You might not set the URI correctly!", Toast.LENGTH_LONG).show();
+                }
+                mPlayer.start();
             }
         });
     }
